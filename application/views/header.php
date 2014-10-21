3f3f564c22cba6dcce9f659d73599e5ca7693966
Nitipatt <iam.pae0@gmail.com>
2014-10-22 02:28:25 +0700

    logo

--- application/views/header.php
+++ application/views/header.php
@@ -13,7 +13,7 @@
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-        <a class="navbar-brand" href="../pages">Trading in your hand</a> </div>
+        <a class="navbar-brand" href="../pages" onMouseOver="logo.src='../../images/logo2.png';" onMouseOut="logo.src='../../images/logo.png';"><img src="../../images/logo.png" width="25px" height="25px" id="logo"/>Trading in your hand</a> </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
