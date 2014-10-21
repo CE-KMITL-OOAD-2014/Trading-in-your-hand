836b537c1cb7c2887cb317fd75344009f64507a2
Nitipatt <iam.pae0@gmail.com>
2014-10-21 23:32:10 +0700

    body gradient

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -3,20 +3,29 @@
 class pages extends CI_Controller {
 	public function index()
 	{
+		$this->load->helper('body.php');
 		$this->load->view('header.php');
 		$this->load->view('home.php');
 	}
 	public function register()
 	{
+		$this->load->helper('body.php');
+		$this->load->view('header.php');
 		$this->load->view('register.php');
 	}
 	public function login(){
+		$this->load->helper('body.php');
+		$this->load->view('header.php');
 		$this->load->view('login.php');	
 	}
 	public function addproduct(){
+		$this->load->helper('body.php');
+		$this->load->view('header.php');
 		$this->load->view('add_product.php');	
 	}
 	public function editproduct(){
+		$this->load->helper('body.php');
+		$this->load->view('header.php');
 		$this->load->view('edit_product.php');	
 	}
 	public function displayproduct(){
@@ -24,6 +33,8 @@ class pages extends CI_Controller {
 		$this->load->model('Product_model');	
 		$data = $this->Product_model->display_product();
 		$data_send['data'] = $data;
+		$this->load->helper('body.php');
+		$this->load->view('header.php');
 		$this->load->view('display_product.php',$data_send);
 	}
 }
---
+++ application/helpers/body.php
@@ -0,0 +1,7 @@
+<?
+echo"<style>
+body {background-color: -webkit-linear-gradient(#CCC, white); /* For Safari 5.1 to 6.0 */
+     background-color: -o-linear-gradient(#CCC, white); /* For Opera 11.1 to 12.0 */
+  	 background-color: -moz-linear-gradient(#CCC, white); /* For Firefox 3.6 to 15 */
+  	 background-color: linear-gradient(#CCC, white); /* Standard syntax */}</style>";
+?>
