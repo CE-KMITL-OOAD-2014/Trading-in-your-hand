67ce7925f64211afa6e7187be4eff49e2b99084c
Nitipatt <iam.pae0@gmail.com>
2014-10-22 01:15:24 +0700

    try

--- application/views/header.php
+++ application/views/header.php
@@ -8,6 +8,7 @@
 </head>
 <body>
 <div style="background-color:#CCC">
+<<<<<<< HEAD
 <nav class="navbar navbar-inverse" role="navigation" style="border-radius:0 !important; background-color:#666 !important">
 <div class="container-fluid"> 
   <!-- Brand and toggle get grouped for better mobile display -->
@@ -42,4 +43,48 @@
   </div>
   <!-- /.navbar-collapse --> 
 </div>
+=======
+  <nav class="navbar navbar-inverse" role="navigation" style="border-radius:0 !important; background-color:#666 !important">
+    <div class="container-fluid"> 
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a class="navbar-brand" href="../pages">Trading in your hand</a> </div>
+      
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+        </ul>
+        <form class="navbar-form navbar-left" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Product name">
+          </div>
+          <button type="submit" class="btn btn-default">Search</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">Promotion</a></li>
+          <li><a href="#">About</a></li>
+          <li class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"> Dropdown <span class="caret"></span> </button>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+              <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </nav>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+  <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <script src="../../js/bootstrap.min.js"></script> 
+</div>
+</body>
+>>>>>>> parent of b015185... sssssssssssssss
 </html>
