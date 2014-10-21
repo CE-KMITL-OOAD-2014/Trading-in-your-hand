27c4c6bab2d261017b935d5e5fe7a7e41a640472
Nitipatt <iam.pae0@gmail.com>
2014-10-21 23:18:34 +0700

    edit test

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
     	<nav class="navbar navbar-inverse" role="navigation" style="border-radius:0 !important; background-color:#666 !important">
@@ -53,6 +53,6 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
