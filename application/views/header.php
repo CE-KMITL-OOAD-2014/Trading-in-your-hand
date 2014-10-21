69c4355e2033c1da7e4f757af205c98ae67074fc
Nitipatt <iam.pae0@gmail.com>
2014-10-21 21:59:23 +0700

    adsf

--- application/views/header.php
+++ application/views/header.php
@@ -50,19 +50,49 @@ body{background:#E1E1E1;
 	}
 </style>
 <html>
-<head>
-<meta charset="utf-8"/>
-<title>Home</title>
-</head>
-<body>
-<div class="top-menu">
-	<ul>
-		<li><a href='<? echo"".$menuone; ?>'>Home</a></li>
-		<li><a href='<? echo"".$menutwo; ?>'>Product</a></li>
-		<li><a href='<? echo"".$menuthree; ?>'>About</a></li>
-		
-        <li><img src="Search.png" style="height:50%; width:auto; magin-top:2.2%"/></li>
-	</ul>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+        <div class="top-menu">
+            <ul>
+                <li><a href='<? echo"".$menuone; ?>'>Home</a></li>
+                <li><a href='<? echo"".$menutwo; ?>'><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"></button>product</a></li>
+                <li><a href='<? echo"".$menuthree; ?>'>About</a></li>
+                
+                <li><img src="Search.png" style="height:50%; width:auto; magin-top:2.2%"/></li>
+            </ul>
+        </div>
+        
+        
+        
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
-</body>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+        </body>
 </html>
