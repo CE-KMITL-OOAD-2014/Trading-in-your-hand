19b5653b2b607fdd2ac406728b4ecae0acf7fbb5
Nitipatt <iam.pae0@gmail.com>
2014-10-22 05:45:56 +0700

    sfdgh

--- application/views/register.php
+++ application/views/register.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
-  <body><div class="back"> 
-   
-    <script>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap 101 Template</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="back"> 
+  <script>
    $(function () {
     $('.button-checkbox').each(function () {
 
@@ -77,102 +76,93 @@
     });
 });
    
-   </script>
-   
-   
-   
-   <!--------------------------Login------------------------------------------------------------------------------------------------------------>
-<div class="container">
-
-<div class="row">
-    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-		<form role="form">
-			<h2>Please Sign Up <small>It's free and always will be.</small></h2>
-			<hr class="colorgraph">
-			<div class="row">
-					<div class="form-group">
-						<input type="text" name="id" id="id" class="form-control input-lg" placeholder="username" tabindex="1" required autofocus>
-					</div>
-                	<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="text" name="pass" id="pass" class="form-control input-lg" placeholder="Password" tabindex="2" required autofocus>
-					</div>
-				</div>
-                
-                
-                
-                <div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-                        <input type="text" name="name" id="name" class="form-control input-lg" placeholder="First Name" tabindex="3" required autofocus>
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="text" name="sname" id="sname" class="form-control input-lg" placeholder="Last Name" tabindex="4" required autofocus>
-					</div>
-				</div>
-			</div>
-		
-			<div class="form-group">
-				<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="5" required autofocus>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="6" required autofocus>
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7">
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 col-sm-3 col-md-3">
-					<span class="button-checkbox">
-						<button type="button" class="btn" data-color="info" tabindex="8">I Agree</button>
-                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
-					</span>
-				</div>
-				<div class="col-xs-8 col-sm-9 col-md-9">
-					 By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="../member/register" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
-				</div>
-			</div>
-			
-			<hr class="colorgraph">
-			<div class="row">
-				<div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
-				<div class="col-xs-12 col-md-6"><a href="../pages/login" class="btn btn-success btn-block btn-lg">Sign In</a></div>
-			</div>
-		</form>
-	</div>
-</div>
-<!-- Modal -->
-<div class="modal fade" id="t_and_c_m" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-lg">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">?</button>
-				<h4 class="modal-title" id="myModalLabel">Terms & Conditions</h4>
-			</div>
-		
-			<div class="modal-footer">
-				<button type="button" class="btn btn-primary" data-dismiss="modal">I Agree</button>
-			</div>
-		</div><!-- /.modal-content -->
-	</div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-</div>
-
-<!-----------------------------------End_Login---------------------------------------------------------------------------------------------------->
+   </script> 
   
-   
-   
-   
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+  <!--------------------------Login------------------------------------------------------------------------------------------------------------>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+        <form role="form">
+          <h2>Please Sign Up <small>It's free and always will be.</small></h2>
+          <hr class="colorgraph">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                <input type="text" name="id" id="id" class="form-control input-lg" placeholder="username" tabindex="1" required autofocus>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="form-group">
+                <input type="text" name="pass" id="pass" class="form-control input-lg" placeholder="Password" tabindex="2" required autofocus>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="form-group">
+                <input type="text" name="name" id="name" class="form-control input-lg" placeholder="First Name" tabindex="3" required autofocus>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="form-group">
+                <input type="text" name="sname" id="sname" class="form-control input-lg" placeholder="Last Name" tabindex="4" required autofocus>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="5" required autofocus>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="form-group">
+                <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="6" required autofocus>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="form-group">
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-4 col-sm-3 col-md-3"> <span class="button-checkbox">
+              <button type="button" class="btn" data-color="info" tabindex="8">I Agree</button>
+              <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
+              </span> </div>
+            <div class="col-xs-8 col-sm-9 col-md-9"> By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="../member/register" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use. </div>
+          </div>
+          <hr class="colorgraph">
+          <div class="row">
+            <div class="col-xs-12 col-md-6">
+              <input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7">
+            </div>
+            <div class="col-xs-12 col-md-6"><a href="../pages/login" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+          </div>
+        </form>
+      </div>
     </div>
-  </body>
+    <!-- Modal -->
+    <div class="modal fade" id="t_and_c_m" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">?</button>
+            <h4 class="modal-title" id="myModalLabel">Terms & Conditions</h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">I Agree</button>
+          </div>
+        </div>
+        <!-- /.modal-content --> 
+      </div>
+      <!-- /.modal-dialog --> 
+    </div>
+    <!-- /.modal --> 
+  </div>
+  
+  <!-----------------------------------End_Login----------------------------------------------------------------------------------------------------> 
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+  <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <script src="js/bootstrap.min.js"></script> 
+</div>
+</body>
 </html>
