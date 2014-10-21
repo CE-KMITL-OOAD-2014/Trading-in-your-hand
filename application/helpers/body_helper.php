0ae0bec356190b57118690013a42f41ff7e10a73
Nitipatt <iam.pae0@gmail.com>
2014-10-22 00:01:41 +0700

    edit

--- application/helpers/body_helper.php
+++ application/helpers/body_helper.php
@@ -1,7 +1,7 @@
 <?
 echo"<style>
-.back{background: -webkit-linear-gradient(#CCC, white); /* For Safari 5.1 to 6.0 */
- 	  background: -o-linear-gradient(#CCC, white); /* For Opera 11.1 to 12.0 */
- 	  background: -moz-linear-gradient(#CCC, white); /* For Firefox 3.6 to 15 */
- 	  background: linear-gradient(#CCC, white); /* Standard syntax */}</style>";
+.back{background: -webkit-linear-gradient(white,#CCC); /* For Safari 5.1 to 6.0 */
+ 	  background: -o-linear-gradient(white,#CCC); /* For Opera 11.1 to 12.0 */
+ 	  background: -moz-linear-gradient(white,#CCC); /* For Firefox 3.6 to 15 */
+ 	  background: linear-gradient(white,#CCC); /* Standard syntax */}</style>";
 ?>
