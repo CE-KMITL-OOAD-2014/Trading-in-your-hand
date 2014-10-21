7eb49caadfbeab6e1347965c8167022faf12ed8f
Nitipatt <iam.pae0@gmail.com>
2014-10-22 05:35:29 +0700

    fdgfdgdf

--- application/views/register.php
+++ application/views/register.php
@@ -8,7 +8,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
   </head>
-  <body>
+  <body><div class="back"> 
    
     <script>
    $(function () {
@@ -175,5 +175,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    </div>
   </body>
 </html>
