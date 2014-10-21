b6f97bf53db598fa97a0c048182c75e8418874d9
Nitipatt <iam.pae0@gmail.com>
2014-10-22 04:11:03 +0700

    asdfsadf

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
-
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
-  <body>
-    <div class="container">    
-    	<div class="row-fluid">
-		<div class="span12">
-			<div class="span8">
-				<a href="#">Terms of Service</a>    
-				<a href="#">Privacy</a>    
-				<a href="#">Security</a>
-			</div>
-			<div class="span4">
-				<p class="muted pull-right">© 2013 Company Name. All rights reserved</p>
-			</div>
-		</div>
-	</div>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap 101 Template</title>
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+  <div class="row-fluid">
+    <div class="navbar navbar-fixed-bottom">
+      <div class="span12">
+        <div class="span8"> <a href="#">Terms of Service</a> <a href="#">Privacy</a> <a href="#">Security</a> </div>
+        <div class="span4">
+          <p class="muted pull-right">© 2013 Company Name. All rights reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/bootstrap.min.js"></script>
-  </body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="../js/bootstrap.min.js"></script>
+</body>
 </html>
