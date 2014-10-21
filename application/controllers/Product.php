8a1593bef19aa54368bfdc68e792c4d22950aeef
Nitipatt <iam.pae0@gmail.com>
2014-10-21 19:33:28 +0700

    test

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -5,11 +5,12 @@
 		
 		}
 		function add(){
+			$id = 55;
 			$name = $_POST["name"];
 			$price = $_POST["price"];
 			$amount = $_POST["amount"];
 			$username = "admin"; // Get username from session
-			$data = array('name'=>$name,'price'=>$price,'amount'=>$amount,'owner'=>$username);
+			$data = array('id'=>$id,'name'=>$name,'price'=>$price,'amount'=>$amount,'owner'=>$username);
 			$this->load->model('Product_model');	
 			if($this->Product_model->add_product($data)){
 				echo"<h1>Your product has added to database</h1><br />";
