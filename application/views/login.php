87430d47e96847e017b008a0b6686923578af1ab
Nitipatt <iam.pae0@gmail.com>
2014-10-22 00:07:23 +0700

    ewr

--- application/views/login.php
+++ application/views/login.php
@@ -18,7 +18,6 @@
    <section id="login">
     <div class="container">
     	<div class="row">
-    	    <div class="col-xs-12">
         	    <div class="col-xs-3"></div>
                 <div class="col-xs-3"></div>
                 <div class="col-xs-3">
@@ -40,7 +39,6 @@
                     <hr>
         	    </div>
                 </div>
-    		</div> <!-- /.col-xs-12 -->
     	</div> <!-- /.row -->
     </div> <!-- /.container -->
 </section>
