0002d9375d87bb26c93bee13fe71880ada2d6797
Nitipatt <iam.pae0@gmail.com>
2014-10-22 18:15:19 +0700

    update homepage

--- application/views/home.php
+++ application/views/home.php
@@ -6,50 +6,93 @@
 <title>Bootstrap 101 Template</title>
 <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body><div class="back">
-<br/>
-<br/>
-<section class="section-white">
-  <div class="container">
-  <div class="col-md-2"></div>
-  <div class="col-md-8">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"> 
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
-      
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active"> <a href="#"><img src="http://placehold.it/800x400" alt="..."/></a>
-          <div class="carousel-caption">
-            <h2>Heading</h2>
+<body>
+<div class="back"> <br/>
+  <br/>
+  <section class="section-white">
+    <div class="container">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"> 
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+          
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active"> <a href="#"><img src="http://placehold.it/800x400" alt="..."/></a>
+              <div class="carousel-caption">
+                <h2>Heading</h2>
+              </div>
+            </div>
+            <div class="item"> <a href="#"><img src="http://placehold.it/800x400" alt="..."></a>
+              <div class="carousel-caption">
+                <h2>Heading</h2>
+              </div>
+            </div>
+            <div class="item"> <a href="#"><img src="http://placehold.it/800x400" alt="..."></a>
+              <div class="carousel-caption">
+                <h2>Heading</h2>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="item"> <a href="#"><img src="http://placehold.it/800x400" alt="..."></a>
-          <div class="carousel-caption">
-            <h2>Heading</h2>
-          </div>
-        </div>
-        <div class="item"> <a href="#"><img src="http://placehold.it/800x400" alt="..."></a>
-          <div class="carousel-caption">
-            <h2>Heading</h2>
-          </div>
-        </div>
-      </div>
-      
-      <!-- Controls --> 
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span> </a> <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span> </a> </div>
+          
+          <!-- Controls --> 
+          <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span> </a> <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span> </a> </div>
       </div>
       <div class="col-md-2"></div>
+    </div>
+  </section>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+  <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <script src="js/bootstrap.min.js"></script>
+  <div class="container" id="tourpackages-carousel">
+    <div class="row">
+      <div class="col-xs-18 col-sm-6 col-md-3">
+        <div class="thumbnail"> <img src="http://placehold.it/500x300" alt="">
+          <div class="caption">
+            <h4>Thumbnail label</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+            <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-18 col-sm-6 col-md-3">
+        <div class="thumbnail"> <img src="http://placehold.it/500x300" alt="">
+          <div class="caption">
+            <h4>Thumbnail label</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+            <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-18 col-sm-6 col-md-3">
+        <div class="thumbnail"> <img src="http://placehold.it/500x300" alt="">
+          <div class="caption">
+            <h4>Thumbnail label</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+            <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-18 col-sm-6 col-md-3">
+        <div class="thumbnail"> <img src="http://placehold.it/500x300" alt="">
+          <div class="caption">
+            <h4>Thumbnail label</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+            <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End row --> 
+    
   </div>
-</section>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="js/bootstrap.min.js"></script>
+  <!-- End container --> 
 </div>
 </body>
 </html>
