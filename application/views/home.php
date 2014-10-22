fdfc9bb1ff18152a9069b48a65067dcfd4337638
Nitipatt <iam.pae0@gmail.com>
2014-10-22 18:17:06 +0700

    fixed homepage

--- application/views/home.php
+++ application/views/home.php
@@ -46,6 +46,9 @@
       <div class="col-md-2"></div>
     </div>
   </section>
+  <br/>
+  <br/>
+  <br/>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
   <!-- Include all compiled plugins (below), or include individual files as needed --> 
