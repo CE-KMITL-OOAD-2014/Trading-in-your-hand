73ee761902fe88ea6a7ccfbc6c8b5f211a8ae155
Nitipatt <iam.pae0@gmail.com>
2014-10-22 19:27:07 +0700

    fixed title

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,14 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<?
+if($page=="home")
+	echo"<title>Trading in your hand</title>";
+else if($page=="register")
+	echo"<title>Register - Trading in your hand</title>";
+else if($page=="login")
+	echo"<title>Log in - Trading in your hand</title>";
+?>
 <link href="../../css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
