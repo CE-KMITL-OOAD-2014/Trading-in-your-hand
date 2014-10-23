f13bd5788fa35af2244b16b687090814c132f2e9
Nitipatt <iam.pae0@gmail.com>
2014-10-23 23:50:45 +0700

    bug fixed

--- application/views/header.php
+++ application/views/header.php
@@ -24,6 +24,7 @@ else if($page=="login")
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <form class="navbar-form navbar-left" role="search">
+        <div class="row">
           <div class="form-group">
             <input type="text" class="form-control" placeholder="Product name">
           </div>
@@ -41,6 +42,7 @@ else if($page=="login")
             </ul>
           </div>
           <button type="submit" class="btn btn-default">Search</button>
+          </div>
         </form>
         <ul class="nav navbar-nav navbar-right">
           <? if($page=="home")
