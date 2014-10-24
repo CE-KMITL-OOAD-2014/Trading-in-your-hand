5108cb0e2cab10043ddb51a71d4b634b98b8ed0a
Nitipatt <iam.pae0@gmail.com>
2014-10-25 00:57:38 +0700

    bug fix

--- application/views/message.php
+++ application/views/message.php
@@ -1,15 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="back"> 
 	<form action="../member/sendmessage" method="post">
     	<input type="text" id = "receiver" name="receiver" />
         <input type="text" id = "message" name = "message" />
         <input type="submit"/>    	
     </form>
     <a href="../member/viewmessage">View message</a>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+  <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <script src="../../js/bootstrap.min.js"></script> 
+</div>
 </body>
 </html>
