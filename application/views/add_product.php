a16fe8fe7c7ec0f05c92ba62b74e87ca5d6992bb
Nitipatt <iam.pae0@gmail.com>
2014-10-26 05:40:31 +0700

    fixed

--- application/views/add_product.php
+++ application/views/add_product.php
@@ -8,52 +8,52 @@
 <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<div class ="back"> 
-  
-  <!-------------------------------show product--------------------------------------------------------------------------------------------------->
-  
-  <div class = "container">
-    <div col-md-12>
-      <div>
-        <div class="col-md-2"></div>
-        <h3> Add Product</h3>
-      </div>
+<div class ="back">
+
+<!-------------------------------show product--------------------------------------------------------------------------------------------------->
+
+<div class = "container">
+  <div col-md-12>
+    <div>
       <div class="col-md-2"></div>
-      <div class="col-md-4"> <a class="thumbnail" > <img src="http://fc07.deviantart.net/fs70/i/2012/358/7/2/_rurouni_kenshin__art_of_sword_drawing__by_eyjaynizel-d5p0jtr.jpg"></a> </div>
-      <div class="col-md-4">
+      <h3> Add Product</h3>
+    </div>
+    <div class="col-md-2"></div>
+    <div class="col-md-4"> <a class="thumbnail" > <img src="http://fc07.deviantart.net/fs70/i/2012/358/7/2/_rurouni_kenshin__art_of_sword_drawing__by_eyjaynizel-d5p0jtr.jpg"></a> </div>
+    <div class="col-md-4">
       <form role="form" action="../pages/addproduct" method="post" id="addproduct" autocomplete="off">
-       <div class="form-group">
-        <label for="name" class="sr-only">name</label>
-        <input type="name" name="name" id="name" class="form-control"style="background-color:#FFC" placeholder="NameProduct" required autofocus>
+        <div class="form-group">
+          <label for="name" class="sr-only">name</label>
+          <input type="name" name="name" id="name" class="form-control"style="background-color:#FFC" placeholder="NameProduct" required autofocus>
         </div>
-         <div class="form-group">
-        <label for="cost" class="sr-only">cost</label>
-        <input type="cost" name="cost" id="cost" class="form-control"style="background-color:#FFC" placeholder="Cost" required autofocus>
+        <div class="form-group">
+          <label for="cost" class="sr-only">cost</label>
+          <input type="cost" name="cost" id="cost" class="form-control"style="background-color:#FFC" placeholder="Cost" required autofocus>
         </div>
-         <div class="form-group">
-        <label for="Amount" class="sr-only">Amount</label>
-        <input type="Amount" name="Amount" id="Amount" class="form-control"style="background-color:#FFC" placeholder="Amount" required autofocus>
+        <div class="form-group">
+          <label for="Amount" class="sr-only">Amount</label>
+          <input type="Amount" name="Amount" id="Amount" class="form-control"style="background-color:#FFC" placeholder="Amount" required autofocus>
         </div>
         <div class="form-group">
           <textarea rows="8" class="form-control" placeholder="Detail" style="resize:none;background-color:#FFC" required autofocus ></textarea>
         </div>
-        </form>
-      </div>
-      <div class="row hidden-xs" >
-        <div class="col-md-12">
-          <div class="col-md-2"></div>
-          <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-1"><img src="http://fc04.deviantart.net/fs70/i/2012/281/d/f/himura_kenshin_by_hao_hime-d5h6ebm.png"></a> </div>
-          <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-2"><img src="http://static.comicvine.com/uploads/original/12/128032/3191314-2192489702-46575.jpg"></a> </div>
-          <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/170x100&text=four"></a> </div>
-          <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/170x100&text=five"></a> </div>
-        </div>
+      </form>
+    </div>
+    <div class="row hidden-xs" >
+      <div class="col-md-12">
+        <div class="col-md-2"></div>
+        <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-1"><img src="http://fc04.deviantart.net/fs70/i/2012/281/d/f/himura_kenshin_by_hao_hime-d5h6ebm.png"></a> </div>
+        <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-2"><img src="http://static.comicvine.com/uploads/original/12/128032/3191314-2192489702-46575.jpg"></a> </div>
+        <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/170x100&text=four"></a> </div>
+        <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/170x100&text=five"></a> </div>
       </div>
     </div>
-    <!-----------------------------------End_show product---------------------------------------------------------------------------------------------> 
-    <div class="col-md-10">
-    <div class="pull-right">
-    <button type="button" class="btn btn-default btn-lg" style="background-color:#0CF" id="addproduct" value="Submit"/>
   </div>
+  <!-----------------------------------End_show product--------------------------------------------------------------------------------------------->
+  <div class="col-md-10">
+    <div class="pull-right">
+      <button type="button" class="btn btn-default btn-lg" id="addproduct" value="Submit">Submit</button>>
+    </div>
   </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
