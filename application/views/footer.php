e06ecbc9a633da3398ce97dd0ab599c6714fdf9e
Nitipatt <iam.pae0@gmail.com>
2014-10-26 05:08:14 +0700

    test

--- application/views/footer.php
+++ application/views/footer.php
@@ -11,13 +11,15 @@
 <br/>
 <div class="container">
   <div class="navbar navbar-bottom">
-      <div class="span12">
-        <div class="col-md-8"></div>
-        <div class="col-md-3">
+    <div class="span12">
+      <div class="col-md-8"></div>
+      <div class="col-md-3">
+        <div class= "foot">
           <p class="muted">© 2014 Forkbomb. All rights reserved</p>
         </div>
-        <div class="col-md-1"></div>
       </div>
+      <div class="col-md-1"></div>
+    </div>
   </div>
 </div>
 
