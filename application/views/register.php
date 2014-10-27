7a180622f0d899471ab18b2be0d445aa539741c2
Nitipatt <iam.pae0@gmail.com>
2014-10-27 20:21:11 +0700

    restore

--- application/views/register.php
+++ application/views/register.php
@@ -8,37 +8,7 @@
 </head>
 <body>
 <div class="back"> 
-  <script>
-  function checkPass()
-{
-    //Store the password field objects into variables ...
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
-    //Store the Confimation Message Object ...
-    var message = document.getElementById('confirmMessage');
-    //Set the colors we will be using ...
-    var goodColor = "#66cc66";
-    var badColor = "#ff6666";
-    //Compare the values in the password field
-    //and the confirmation field
-    if(pass1.value == pass2.value){
-        //The passwords match.
-        //Set the color to the good color and inform
-        //the user that they have entered the correct password
-        pass2.style.backgroundColor = goodColor;
-        message.style.color = goodColor;
-        message.innerHTML = "Passwords Match!"
-    }else{
-        //The passwords do not match.
-        //Set the color to the bad color and
-        //notify the user.
-        pass2.style.backgroundColor = badColor;
-        message.style.color = badColor;
-        message.innerHTML = "Passwords Do Not Match!"
-    }
-}  
-  
-  
+  <script> 
    $(function () {
     $('.button-checkbox').each(function () {
 
@@ -145,7 +115,7 @@
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6">
               <div class="form-group">
-                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7" onkeyup="checkPass(); return false;" required autofocus>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7" required autofocus>
               </div>
             </div>
           </div>
