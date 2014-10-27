ec3f6eb7fef284b57be07a71b843506067188d85
Nitipatt <iam.pae0@gmail.com>
2014-10-27 23:36:50 +0700

    test

--- application/views/register.php
+++ application/views/register.php
@@ -71,14 +71,18 @@ $(document).ready(function () {
               <div class="form-group">
                 <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7" required autofocus>
               </div>
-              <div class="row">
-               <input type="button" value="Register" id="regisbutton" class="btn btn-primary btn-block btn-lg" tabindex="7">
-               <div class="col-xs-12 col-md-6"><a href="../pages/login" class="btn btn-success btn-block btn-lg">Sign In</a></div>
-               </div>
             </div>
           </div>
-         
-         
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12"> By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="../member/register" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use. </div>
+          </div>
+          <hr class="colorgraph">
+          <div class="row">
+            <div class="col-xs-12 col-md-6">
+              <input type="button" value="Register" id="regisbutton" class="btn btn-primary btn-block btn-lg" tabindex="7">
+            </div>
+            <div class="col-xs-12 col-md-6"><a href="../pages/login" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+          </div>
         </form>
       </div> <!--col md -->
     </div> <!-- row -->
