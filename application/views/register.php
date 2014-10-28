a28c21fa3897267921d67dea0be2a4a56923c76d
Nitipatt <iam.pae0@gmail.com>
2014-10-28 22:43:50 +0700

    test

--- application/views/register.php
+++ application/views/register.php
@@ -32,7 +32,7 @@ $(document).ready(function () {
    
 function validateusername(){
 		var regex = /^[a-zA-Z0-9]{4,12}$/;
-		var text = $("#username").text();
+		var text = $("#username").val();
 		alert(regex.test(text));
 	}
    </script> 
