d9d74ed8c5a50145c3dea1c648725699cf872088
Nitipatt <iam.pae0@gmail.com>
2014-10-29 07:08:49 +0700

    fixed2

--- application/views/profile.php
+++ application/views/profile.php
@@ -145,7 +145,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="../../css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="back">
@@ -182,7 +182,7 @@
 	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="js/bootstrap.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
 </div>
 </body>
 </html>
