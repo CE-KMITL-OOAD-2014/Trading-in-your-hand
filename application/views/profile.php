6f136870dc169951378e0066bbfee5190d5b96e2
Nitipatt <iam.pae0@gmail.com>
2014-10-30 00:34:06 +0700

    test2

--- application/views/profile.php
+++ application/views/profile.php
@@ -212,8 +212,10 @@ if($this->session->userdata('username')){
             <p><? echo"".$about.""; ?></p>
             <div class="social"> <a href="<? echo"".$facebook.""; ?>" class="[ social-icon facebook ] animate"><span class="fa fa-facebook"></span></a> <a href="<? echo"".$twitter.""; ?>" class="[ social-icon twitter ] animate"><span class="fa fa-twitter"></span></a> <a href="<? echo"".$github.""; ?>" class="[ social-icon github ] animate"><span class="fa fa-github-alt"></span></a> <a href="<? echo"".$googleplus.""; ?>" class="[ social-icon google-plus ] animate"><span class="fa fa-google-plus"></span></a>
             <a class="btn btn-default btn-sm" href="#">
-  			<i class="fa fa-cog" id="edit"></i>Edit</a>
-
+            <?
+			if($id==$username)
+  			echo"<i class='fa fa-cog' id='edit'></i>Edit</a>";
+			?>
             </div>
           </div>
         </div>
