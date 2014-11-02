c9875f2efed2c899bd3a7f28c53d076b567e1129
Nitipatt <iam.pae0@gmail.com>
2014-11-02 17:28:50 +0700

    test4

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -71,15 +71,15 @@ class member extends CI_Controller {
 	public function edit(){
 		$data = $this->session->all_userdata();
 		$this->load->model('member_model');
-		$data['name'] = $_get["name"];
-		$data['sname'] = $_get["sname"];
-		$data['about'] = $_get["about"];
-		$data['email'] = $_get["email"];
-		$data['address'] = $_get["address"];
-		$data['facebook'] = $_get["facebook"];
-		$data['twitter'] = $_get["twitter"];
-		$data['googleplus'] = $_get["googleplus"];
-		$data['github'] = $_get["github"];
+		$data['name'] = $_GET["name"];
+		$data['sname'] = $_GET["sname"];
+		$data['about'] = $_GET["about"];
+		$data['email'] = $_GET["email"];
+		$data['address'] = $_GET["address"];
+		$data['facebook'] = $_GET["facebook"];
+		$data['twitter'] = $_GET["twitter"];
+		$data['googleplus'] = $_GET["googleplus"];
+		$data['github'] = $_GET["github"];
 		$this->member_model->edit_profile($data);
 		$config =  array(
 			  'file_name'		=> md5(base64_encode($data['username'])),
