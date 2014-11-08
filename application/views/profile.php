955e202b81dfcc92c4516115f0220a9d93ecbcaf
Nitipatt <iam.pae0@gmail.com>
2014-11-09 05:45:50 +0700

    test3

--- application/views/profile.php
+++ application/views/profile.php
@@ -303,17 +303,26 @@ a.linkedin:hover {
         <!--class row--> 
       </div>
       <?
+	  $now = int($ppage/5);
+	  echo $now;
 	  echo"
       <div class='text-center'><ul class='pagination pagination-large'>
 		<li><a href='";
-		if($numpage==1)echo"#";
-		else echo"../../../../../pages/member/".$id."/2";
-		echo"' rel='prev'>&laquo;</a></li>
-        <li class='active'><span>1</span></li>
-        <li><a href='#'>2</a></li>
-        <li><a href='#'>3</a></li>
-        <li><a href='#'>4</a></li>
-        <li><a href='#'>5</a></li>
+		if($ppage==1)echo"#";
+		else echo"../../../../../pages/member/".$id."/".$ppage-1;
+		echo"' rel='prev'>&laquo;</a></li>";
+		if($ppage%5 == 1)echo"<li class='active'><span>".$ppage."</span></li>";
+		else echo"<li><a href='../../../../../pages/member/".$id."/1'>".$ppage."</a></li>";
+		if($ppage%5 == 2)echo"<li class='active'><span>".$ppage."</span></li>";
+		else echo"<li><a href='../../../../../pages/member/".$id."/1'>".$ppage."</a></li>";
+        if($ppage%5 == 3)echo"<li class='active'><span>".$ppage."</span></li>";
+		else echo"<li><a href='../../../../../pages/member/".$id."/1'>".$ppage."</a></li>";
+        if($ppage%5 == 4)echo"<li class='active'><span>".$ppage."</span></li>";
+		else echo"<li><a href='../../../../../pages/member/".$id."/1'>".$ppage."</a></li>";
+        if($ppage%5 == 5)echo"<li class='active'><span>".$ppage."</span></li>";
+		else echo"<li><a href='../../../../../pages/member/".$id."/1'>".$ppage."</a></li>";
+		
+		echo"
         <li class='disabled'><span>...</span></li>
         <li><a href='#'>last</a></li>
         <li><a href='#' rel='next'>&raquo;</a></li>	</ul>
