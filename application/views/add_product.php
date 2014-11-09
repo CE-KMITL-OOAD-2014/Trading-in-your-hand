573f95fac6b7439b608f22650180cb9b39b66e21
Nitipatt <iam.pae0@gmail.com>
2014-11-09 20:00:11 +0700

    test3

--- application/views/add_product.php
+++ application/views/add_product.php
@@ -21,30 +21,29 @@
       <div class="col-md-2"></div>
       <div class="col-md-4"> <a class="thumbnail" > <img src="http://fc07.deviantart.net/fs70/i/2012/358/7/2/_rurouni_kenshin__art_of_sword_drawing__by_eyjaynizel-d5p0jtr.jpg"></a> </div>
       <div class="col-md-4">
+      <form action="#" method="post" id="data" name="data">
         <label for="name" class="sr-only">name</label>
-        <input type="name" name="name" id="name" class="form-control"style="background-color:#FFC" placeholder="NameProduct" required autofocus>
+        <input type="name" name="name" id="name" class="form-control" placeholder="NameProduct" required autofocus>
         <label for="cost" class="sr-only">cost</label>
-        <input type="cost" name="cost" id="cost" class="form-control"style="background-color:#FFC" placeholder="Cost" required autofocus>
+        <input type="cost" name="cost" id="cost" class="form-control" placeholder="Cost" required autofocus>
         <label for="Amount" class="sr-only">Amount</label>
-        <input type="Amount" name="Amount" id="Amount" class="form-control"style="background-color:#FFC" placeholder="Amount" required autofocus>
+        <input type="Amount" name="Amount" id="Amount" class="form-control" placeholder="Amount" required autofocus>
         <div class="form-group">
-          <textarea rows="8" class="form-control" placeholder="Detail" style="resize:none;background-color:#FFC" required autofocus ></textarea>
+          <textarea rows="8" class="form-control" placeholder="Detail" style="resize:none" required autofocus ></textarea>
         </div>
+        </form>
       </div>
       <div class="row hidden-xs" >
         <div class="col-md-12">
           <div class="col-md-2"></div>
           <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-1"><img src="http://placehold.it/170x100&text=one"></a> </div>
           <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/170x100&text=two"></a> </div>
+          <div class="col-md-2"></div>
+          <button type="submit" class="btn btn-default btn-lg" form = "data">  Submit </button>
         </div>
       </div>
     </div>
     <!---->
-    <div class="col-md-10">
-    <div class="pull-right">
-    <button type="button" class="btn btn-default btn-lg" style="background-color:#0CF" >  Upload </button>
-  </div>
-  </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
