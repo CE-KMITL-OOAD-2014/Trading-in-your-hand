57b6e4d69e2ebf3300156f9c53a21fd01eb3959b
Nitipatt <iam.pae0@gmail.com>
2014-11-09 08:12:40 +0700

    test5

--- application/views/profile.php
+++ application/views/profile.php
@@ -263,7 +263,7 @@ a.linkedin:hover {
 				echo"<button class='btn btn-sm btn-primary'><i class='fa fa-fw fa-shopping-cart'></i> Buy</button>";
             echo"</span> </div></div>";
          //////////////////////////////
-		 if($num >= (($ppage*3)-1)){
+		 if($num > (($ppage*3)-1)){
 		echo"
         <div class='slide-row'>
           <div id='carousel-1' class='carousel slide slide-carousel' data-ride='carousel'>             
@@ -286,7 +286,7 @@ a.linkedin:hover {
             echo"</span> </div></div>";
 			}
          //////////////////
-		 if($num >= ($ppage*3)){
+		 if($num > ($ppage*3)){
 		echo"
         <div class='slide-row'>
           <div id='carousel-1' class='carousel slide slide-carousel' data-ride='carousel'>             
