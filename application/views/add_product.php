f73f7a357bdc1f5ed4cc67e4966fdb4c9c96275c
Nitipatt <iam.pae0@gmail.com>
2014-11-09 20:05:26 +0700

    test5

--- application/views/add_product.php
+++ application/views/add_product.php
@@ -40,6 +40,7 @@
           <div class="col-md-2"> <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/170x100&text=two"></a> </div>
           <div class="col-md-2"></div>
           <button type="submit" class="btn btn-default btn-lg pull-right" form = "data">  Submit </button>
+          <div class="col-md-2"></div>
         </div>
       </div>
     </div>
