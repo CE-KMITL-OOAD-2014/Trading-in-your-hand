783845c5adf1209a99905d53da11e13881a9e5aa
Nitipatt <iam.pae0@gmail.com>
2014-11-12 01:38:38 +0700

    test3

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -1,7 +1,6 @@
 <?
 class member extends CI_Controller {
 	public function twowayauthen($number){
-		$sess = $this->session->all_userdata();
 		$this->load->library('twilio');
 		$from = '+16024836345';
 		$to = $sess['rtel'];
