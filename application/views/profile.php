4651da2e6a76fa5750976067dd0b4ae37cfe67ca
Nitipatt <iam.pae0@gmail.com>
2014-11-11 22:18:28 +0700

    bug fixed2

--- application/views/profile.php
+++ application/views/profile.php
@@ -231,9 +231,9 @@ if($this->session->userdata('username')){
         </div>
       </div>
     <?
-    if($id==$username&&$detail['iden']==1)
+    if($id==$username&&$detail['iden']==1&&$this->session->userdata('username'))
       	echo"<a href='../../../pages/addproduct'><button class='btn btn-success'> Add product</button></a>";
-	else if($id==$username&&$detail['iden']==0)
+	else if($id==$username&&$detail['iden']==0&&$this->session->userdata('username'))
 		echo"<a href='../../../pages/iden'><button class='btn btn-success'> Become a seller</button></a>";
     ?>
 	</div>
