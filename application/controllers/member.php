71233d739fc9cb5a2d3c4c8e628e552ddfba63f4
Nitipatt <iam.pae0@gmail.com>
2014-11-12 19:00:46 +0700

    test

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -29,19 +29,13 @@ class member extends CI_Controller {
 			$address = $_POST['address'];
 			$email = $_POST['email'];
 			$this->load->model('member_model');	
-			if($this->member_model->checkexist($username))
+			if($this->member_model->checkexist($username)){
 				echo"<script language='javascript'>
 	alert('Sorry , There are the exist username in system');
     window.location.href = '../../pages/register';	
-</script>";
-			else{
-			$number = rand(1111111,9999999);
-			$data = array('rusername'=>$username,'rpassword'=>$password,'rname'=>$name,'rsname'=>$sname,'raddress'=>$address,'remail'=>$email,'rcode' => $number);
-			$this->session->set_userdata($data);
-			$this->twowayauthen($number);
-			echo"<script language='javascript'>
-    window.location.href = '../../pages/confirm';	
 </script>";}
+			else{
+			}
 		}
 	public function register2way(){
 			$code = $_POST['code'];
