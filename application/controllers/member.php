795f69f261b27e8f6e4a47b3fa4ed2e178dbacbd
Nitipatt <iam.pae0@gmail.com>
2014-11-13 23:04:35 +0700

    test3

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -3,7 +3,7 @@ class member extends CI_Controller {
 	public function twowayauthen($number){
 		$sess = $this->session->all_userdata();
 		$to = $sess['remail'];
-		$message = "<img src='http://forkbomb.azurewebsites.net/images/headmail.png'/><br/>Dear ".$sess['rusername'].",<br/> Here's the Confirmation code you'll need to complete the process: <br/><h1><b>".$number."</b></h1>";	
+		$message = "<img src='http://forkbomb.azurewebsites.net/images/headmail.png'/><br/><br/>Dear ".$sess['rusername'].",<br/><br/> Here's the Confirmation code you'll need to complete the process: <br/><h1><b>".$number."</b></h1><br/><br/>Thanks for using our website<br/><br/>The Trading in your hand team<br/>Admin : iam.pae0@gmail.com";	
 		$config = Array(
     		'protocol' => 'smtp',
    			'smtp_host' => 'ssl://smtp.googlemail.com',
