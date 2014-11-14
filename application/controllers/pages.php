232d891ec56d080e40e3911a9758d8942aeb2a5f
Nitipatt <iam.pae0@gmail.com>
2014-11-15 04:46:41 +0700

    test35

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -75,14 +75,28 @@ class pages extends CI_Controller {
 		$this->load->view('footer.php');
 	}
 	public function editproduct(){
-		//$this->load->model('Product_model');
-		//		$pdata = $this->Product_model->getproductdetail($this->uri->segment(3));
-		$head['page'] = "Edit product";	
-		$this->load->helper('body.php');
-		$this->load->view('header.php',$head);
-		$this->load->view('space.php');
-		$this->load->view('edit_product.php',$pdata);
-		$this->load->view('footer.php');
+		if($this->session->userdata('username')){
+			$data = $this->session->all_userdata();
+			$id = $data['username'];		
+			if ($this->uri->segment(3) === FALSE){
+				echo"<script language='javascript'>
+		window.location.href = '../../../pages/member/".$data['username']."';
+	</script>";
+			$this->load->model('Product_model');}
+			else{
+				$pdata = $this->Product_model->getproductdetail($this->uri->segment(3));
+				$head['page'] = "Edit product";	
+				$this->load->helper('body.php');
+				$this->load->view('header.php',$head);
+				$this->load->view('space.php');
+				$this->load->view('edit_product.php',$pdata);
+				$this->load->view('footer.php');
+			}
+		}
+		else
+			echo"<script language='javascript'>
+		window.location.href = '../../../pages';
+	</script>";
 	}
 	public function displayproduct(){
 		$this->load->model('Product_model');	
