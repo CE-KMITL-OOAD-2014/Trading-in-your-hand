b90f94beb28ed2c19bec0ca09349a4dad8140cb2
Nitipatt <iam.pae0@gmail.com>
2014-11-15 04:09:19 +0700

    test19

--- application/views/edit_product.php
+++ application/views/edit_product.php
@@ -5,6 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="../../css/bootstrap.min.css" rel="stylesheet">
+<? 			$id = $this->session->all_userdata();
+			$id = $id['username']; ?>
 </head>
 <body>
 <div class="back"> 
@@ -14,38 +16,27 @@
     <div col-md-12>
       <div>
         <div class="col-md-2"></div>
-        <h3> Edit Product</h3>
+        <h3> Add Product</h3>
       </div>
       <div class="col-md-2"></div> 
-      <form action="../../../../product/edit/<? echo $id; ?>" method="post" id="data" name="data" enctype="multipart/form-data">
-      <div class="col-md-4"> <a class="thumbnail" > <img src="../../../productPic/<? echo $pic1; ?>.jpg"></a> 
+      <form action="../../../../product/add" method="post" id="data" name="data" enctype="multipart/form-data">
+      <div class="col-md-4"> <a class="thumbnail" > <img src="../../../productPic/ExampleProductImage.jpg"></a> 
             	<input type="file" name="userfile" class="text-center center-block well well-sm"/>
       </div>
       <div class="col-md-4">
         <label for="name" class="sr-only">name</label>
-        <input type="name" name="name" id="name" class="form-control" placeholder="NameProduct" value="<? echo $name; ?>" required autofocus>
+        <input type="name" name="name" id="name" class="form-control" placeholder="NameProduct" required autofocus>
         <label for="cost" class="sr-only">cost</label>
-        <input type="cost" name="price" id="price" class="form-control" placeholder="Cost" value="<? echo $price; ?>" required autofocus>
+        <input type="cost" name="price" id="price" class="form-control" placeholder="Cost" required autofocus>
         <label for="Amount" class="sr-only">Amount</label>
-        <input type="Amount" name="amount" id="amount" class="form-control" placeholder="Amount" value="<? echo $amount; ?>" required autofocus>
+        <input type="Amount" name="amount" id="amount" class="form-control" placeholder="Amount" required autofocus>
         <select class="form-control" id ="type" name="type">
-        <?
-		if($type!="electronics")
-			echo"<option value='electronics'>Electronics</option>";
-		else
-			echo"<option value='electronics' selected='selected'>Electronics</option>";
-		if($type!="cloths")
-        	echo"<option value='cloths'>Cloths</option>";
-		else
-			echo"<option value='cloths' selected='selected'>Cloths</option>";
-		if($type!="others")
-			echo"<option value='others'>Others</option>";
-		else
-        	echo"<option value='others' selected='selected'>Others</option>";
-		?>
+            <option value="electronics">Electronics</option>
+            <option value="cloths">Cloths</option>
+            <option value="others" selected="selected">Others</option>
 		</select>
         <div class="form-group">
-          <textarea rows="8" class="form-control" id="detail" name="detail" placeholder="Detail" style="resize:none" value="<? echo $detail; ?>" required autofocus ></textarea>
+          <textarea rows="8" class="form-control" id="detail" name="detail" placeholder="Detail" style="resize:none" required autofocus ></textarea>
         </div>
         </div>
         </form>
