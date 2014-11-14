396d7920d1a57d02445f94348c1a6abfc63c6eb8
Nitipatt <iam.pae0@gmail.com>
2014-11-14 12:26:42 +0700

    test3

--- application/views/header.php
+++ application/views/header.php
@@ -22,7 +22,7 @@ else
         <a class="navbar-brand" href="../../../../../pages" onMouseOver="logo.src='../../../../images/logo2.png';" onMouseOut="logo.src='../../../../images/logo.png';"><img src="../../../../images/logo.png" width="25px" height="25px" id="logo"/>Trading in your hand</a> </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <form class="navbar-form navbar-left" role="search">
+        <form class="navbar-form navbar-left" role="search" onkeypress="return event.keyCode != 13;">
           <div class="form-group">
             <input type="text" class="form-control" placeholder="Product name" id="searchname" name="searchname">
           </div>
