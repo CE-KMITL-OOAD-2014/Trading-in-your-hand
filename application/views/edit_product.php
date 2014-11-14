ef56d6f4b3f038d3dca52c7481aa1054dc2d3d43
Nitipatt <iam.pae0@gmail.com>
2014-11-15 05:36:56 +0700

    test51

--- application/views/edit_product.php
+++ application/views/edit_product.php
@@ -14,23 +14,23 @@
         <div class="col-md-4"> <a class="thumbnail" > <img src="../../../productPic/<? echo $pic1; ?>.jpg"></a>
           <input type="file" name="userfile" class="text-center center-block well well-sm"/>
         </div>
-        <div class="col-md-4">
+      
           <div class="form-group">
             <label class="control-label">Name</label>
             <label for="name" class="sr-only">name</label>
             <input type="name" name="name" id="name" class="form-control" placeholder="NameProduct" value="<? echo $name; ?>" required autofocus>
-          </div>
-          <div class="form-group">
+          
+        
             <label class="control-label">Cost</label>
             <label for="cost" class="sr-only">cost</label>
             <input type="cost" name="price" id="price" class="form-control" placeholder="Cost" value="<? echo $price; ?>" required autofocus>
-          </div>
-          <div class="form-group">
+      
+         
             <label class="control-label">Amount</label>
             <label for="Amount" class="sr-only">Amount</label>
             <input type="Amount" name="amount" id="amount" class="form-control" placeholder="Amount" value="<? echo $amount; ?>" required autofocus>
-          </div>
-          <div class="form-group">
+         
+         
             <label class="control-label">Type</label>
             <select class="form-control" id ="type" name="type">
               <?
@@ -48,12 +48,12 @@
         	echo"<option value='others' selected='selected'>Others</option>";
 		?>
             </select>
-          </div>
-          <div class="form-group">
+         
+          
             <label class="control-label">Detail</label>
             <label for="Detail" class="sr-only">Amount</label>
             <input type="Detail" name="detail" id="detail" class="form-control" placeholder="detail" value="<? echo $amount; ?>" required autofocus>
-          </div>
+         
           <div class="form-group">
           <button type="submit" id="sub" class="btn btn-primary btn-block btn-lg pull-right" form = "data"> Submit </button>
         </div>
