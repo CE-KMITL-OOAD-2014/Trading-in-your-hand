af1a35071727083afd375b551e5c21635c573c6d
Nitipatt <iam.pae0@gmail.com>
2014-11-15 06:32:12 +0700

    test8

--- application/views/buy.php
+++ application/views/buy.php
@@ -72,11 +72,6 @@
                              </tr> 
                             
                              <tr>
-                                 <td class="btn-mais-info text-primary">
-                                     <i class="open_info fa fa-plus-square-o"></i>
-                                     <i class="open_info hide fa fa-minus-square-o"></i> informações
-                                 </td>
-                                 <td> </td>
                                  <td class="h5"></td>
                              </tr> 
 
@@ -89,7 +84,7 @@
                     </div>
                     
                     <div class="clearfix"></div>
-                   <p class="open_info hide">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                   <p class="open_info hide"></p>
                   </div>
                     
                   <div class="modal-footer">       
