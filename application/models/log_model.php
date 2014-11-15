c4f3a52b985a78cf0dd6603f1f29e372df485779
Nitipatt <iam.pae0@gmail.com>
2014-11-15 21:52:17 +0700

    test9

--- application/models/log_model.php
+++ application/models/log_model.php
@@ -6,11 +6,11 @@
 		function logtransac($data){
 			$ip = $_SERVER['REMOTE_ADDR'];
 			$time = date("D M d, Y G:i");
-			$this->db->select_max('id');
+			$this->db->select_max('ID');
 			$query = $this->db->get('transaction');
 			foreach($query->result_array() as $row)
 				$id = $row['id']+1;
-			$data = array('id'=>$id,'buyer'=>$data['buyer'],'seller'=>$data['seller'],'product'=>$data['product'],'price'=>$data['price'],'amount'=>$data['amount'],'time'=>$time,'ip'=>$ip,'isScore'=>"0");
+			$data = array('ID'=>$id,'buyer'=>$data['buyer'],'seller'=>$data['seller'],'product'=>$data['product'],'price'=>$data['price'],'amount'=>$data['amount'],'time'=>$time,'ip'=>$ip,'isScore'=>"0");
 			$this->db->insert('transaction',$data);
 		}
 	}
