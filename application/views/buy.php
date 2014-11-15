c4bf3c8506e66ea15c5508baca510991f551e2e5
Nitipatt <iam.pae0@gmail.com>
2014-11-15 07:43:56 +0700

    test33

--- application/views/buy.php
+++ application/views/buy.php
@@ -68,7 +68,7 @@
                              </tr>  
 
                              <tr>
-                                 <td ></td>
+                                 <td >&nbsp;</td>
                              </tr>                            
                             
                             <tr>
