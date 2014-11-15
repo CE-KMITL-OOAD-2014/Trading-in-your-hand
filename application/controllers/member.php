93d195bdb5120ea57c66f51041c72f2d03e792b1
Nitipatt <iam.pae0@gmail.com>
2014-11-16 05:38:54 +0700

    tewst

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -6,9 +6,9 @@ class member extends CI_Controller {
 		$message = "<img src='http://forkbomb.azurewebsites.net/images/headmail.png'/><br/><br/>Dear ".$sess['rusername'].",<br/><br/> Here's the Confirmation code you'll need to complete the process: <br/><h1><b>".$number."</b></h1>If you haven't recently tried to login to Trading in your hand from the device located at ".$_SERVER['REMOTE_ADDR'].", someone else may be trying to access your account.<br/><br/>Thanks for using our website<br/><br/>The Trading in your hand team<br/>Admin : iam.pae0@gmail.com<br/>Co-Admin : nvb_kukuku@hotmail.com";	
 		$config = Array(
     		'protocol' => 'smtp',
-   			'smtp_host' => 'ssl://smtp.googlemail.com',
+   			'smtp_host' => 'ssl://smtp.outlook.com',
     		'smtp_port' => 465,
-    		'smtp_user' => 'test2.trading.in.your.hand@gmail.com',
+    		'smtp_user' => 'test2.trading.in.your.hand@outlook.com',
     		'smtp_pass' => 'pae123456',
     		'mailtype'  => 'html', 
     		'charset'   => 'iso-8859-1'
