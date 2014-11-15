7499deac2d15b494ab03d3223aef607c7fce3f6a
Nitipatt <iam.pae0@gmail.com>
2014-11-16 03:18:27 +0700

    test6

--- application/views/home.php
+++ application/views/home.php
@@ -45,6 +45,7 @@
   <br/>
   <br/>
   <br/>
+  <h2>New arrival.</h2>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
   <!-- Include all compiled plugins (below), or include individual files as needed --> 
