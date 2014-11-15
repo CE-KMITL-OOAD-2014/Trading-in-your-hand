8caf9f51daf659b6ed19832510cd03d18e60e6bc
Nitipatt <iam.pae0@gmail.com>
2014-11-15 18:42:47 +0700

    test57

--- application/views/search.php
+++ application/views/search.php
@@ -119,7 +119,7 @@ function confirm(id){
           <div class='slide-footer'>
 		  <div class='col-md-1'></div>
 		  <div class='col-md-5'>";
-		  echo"Owner :&nbsp;".$data[(($ppage*3)-2)]['username']."<br/>";
+		  echo"<b>Owner :</b>&nbsp;".$data[(($ppage*3)-2)]['username']."<br/>";
 		  if($num!=0)
 		  echo"<b>Price :</b> ".$data[(($ppage*3)-2)]['price']."     <b>Amount :</b> ".$data[(($ppage*3)-2)]['amount']."</div>";
 		  else echo"<b>Price :</b>      <b>Amount :</b> </div>";
@@ -149,7 +149,7 @@ function confirm(id){
           <div class='slide-footer'>
 		  <div class='col-md-1'></div>
 		  <div class='col-md-5'>
-		  ".$data[(($ppage*3)-1)]['username']."<br/>
+		  <b>Owner :</b>&nbsp;".$data[(($ppage*3)-1)]['username']."<br/>
 		  <b>Price :</b> ".$data[(($ppage*3)-1)]['price']."     <b>Amount :</b> ".$data[(($ppage*3)-1)]['amount']."</div>";
 		  echo"<span class='pull-right buttons'>
             <button class='btn btn-sm btn-default'><i class='fa fa-fw fa-eye'></i> Show</button>
@@ -178,7 +178,7 @@ function confirm(id){
           <div class='slide-footer'>
 		  <div class='col-md-1'></div>
 		  <div class='col-md-5'>
-		  ".$data[(($ppage*3))]['username']."<br/>
+		  <b>Owner :</b>&nbsp;".$data[(($ppage*3))]['username']."<br/>
 		  <b>Price :</b> ".$data[($ppage*3)]['price']."     <b>Amount :</b> ".$data[($ppage*3)]['amount']."</div>";
 		  echo"<span class='pull-right buttons'>
             <button class='btn btn-sm btn-default'><i class='fa fa-fw fa-eye'></i> Show</button>
