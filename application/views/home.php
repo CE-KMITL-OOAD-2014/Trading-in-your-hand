38218b968523d748c3114cafb63176fd48ae3f5a
Nitipatt <iam.pae0@gmail.com>
2014-11-16 03:19:38 +0700

    test

--- application/views/home.php
+++ application/views/home.php
@@ -44,8 +44,7 @@
   </section>
   <br/>
   <br/>
-  <br/>
-  <h2>New arrival.</h2>
+   <div class="container"><h2>New arrival.</h2></div>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
   <!-- Include all compiled plugins (below), or include individual files as needed --> 
