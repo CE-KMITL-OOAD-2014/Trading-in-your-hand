6041dbf7b8fcac06922c93eaf91c8a14972ce6e1
Nitipatt <iam.pae0@gmail.com>
2014-11-16 01:11:27 +0700

    test

--- application/views/search.php
+++ application/views/search.php
@@ -202,21 +202,21 @@ function confirm(id){
 			echo"<li><a href='../../../../pages/search/".$type."/".$name."/".($ppage-1)."' rel='prev'>&laquo;</a></li>";	
 			
 		if($numpage>=(5*$x)+2){	
-		if($ppage <= $numpage)
+		if($ppage < $numpage)
 		if($ppage%5 == 1)echo"<li class='active'><span>".$ppage."</span></li>";
 		else echo"<li><a href='../../../../pages/search/".$type."/".$name."/". (($now*5)-4) ."'>". (($now*5)-4) ."</a></li>";
 			if($ppage%5 == 2)echo"<li class='active'><span>".$ppage."</span></li>";
 			else echo"<li><a href='../../../../pages/search/".$type."/".$name."/". (($now*5)-3) ."'>". (($now*5)-3) ."</a></li>";
 			if($numpage>=(5*$x)+3){
-				if($ppage <= $numpage)
+				if($ppage < $numpage)
 				if($ppage%5 == 3)echo"<li class='active'><span>".$ppage."</span></li>";
 				else echo"<li><a href='../../../../pages/search/".$type."/".$name."/". (($now*5)-2) ."'>". (($now*5)-2) ."</a></li>";
 				if($numpage>=(5*$x)+4){
-					if($ppage <= $numpage)
+					if($ppage < $numpage)
 					if($ppage%5 == 4)echo"<li class='active'><span>".$ppage."</span></li>";
 					else echo"<li><a href='../../../../pages/search/".$type."/".$name."/". (($now*5)-1) ."'>". (($now*5)-1) ."</a></li>";
 					if($numpage>=(5*$x)+5){
-						if($ppage <= $numpage)
+						if($ppage < $numpage)
 						if($ppage%5 == 0)echo"<li class='active'><span>".$ppage."</span></li>";
 						else echo"<li><a href='../../../../pages/search/".$type."/".$name."/". ($now*5) ."'>". ($now*5) ."</a></li>";
 					
