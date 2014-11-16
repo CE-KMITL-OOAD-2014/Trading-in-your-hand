e97c01ec105a9566640fb368d31415068535970b
Nitipatt <iam.pae0@gmail.com>
2014-11-16 12:28:48 +0700

    test6

--- application/views/phistory.php
+++ application/views/phistory.php
@@ -32,7 +32,6 @@
           </tbody>
         </table>
       </div>
-      <div class="form-group"> <a class='btn btn-primary btn-block btn-lg pull-right' href='../../../pages/message'>Send Message</a></div>
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
       <!-- Include all compiled plugins (below), or include individual files as needed --> 
