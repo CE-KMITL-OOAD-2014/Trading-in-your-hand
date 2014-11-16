883682d41e43eac1653d413db354c0524025b2e6
Nitipatt <iam.pae0@gmail.com>
2014-11-16 08:57:57 +0700

    test

--- application/views/header.php
+++ application/views/header.php
@@ -32,7 +32,7 @@ else
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <form class="navbar-form navbar-left" role="search" onKeyPress="return event.keyCode != 13;">
           <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search product, member" id="searchname" name="searchname" onkeypress="handle(event);">
+            <input type="text" class="form-control" placeholder="Search product" id="searchname" name="searchname" onKeyPress="handle(event);">
           </div>
           <label style="color:#FFF">Search in</label>
         </form>
